dca4474c296b3403f0910ed7198b7496a3cfa394
Kelly Duran <kaduran16@gmail.com>
2022-09-19 17:24:52 -0400

    rails and react setup with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'sessions/create'
+  post 
   delete 'sessions/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/hello', to: 'application#hello_world' 
