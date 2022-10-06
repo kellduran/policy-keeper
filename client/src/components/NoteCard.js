275d9639eda61dfcfe6c9b13862a2012fa109d88
Kelly Duran <kaduran16@gmail.com>
2022-10-06 05:12:34 -0400

    note editor install stalled

--- client/src/components/NoteCard.js
+++ client/src/components/NoteCard.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { Component} from "react";
+import CKEditor from "@ckeditor/ckeditor5-react";
+import DecoupledcEditor from "@ckeditor/ckeditor5-build-decoupled-document";
+
 
 function NoteCard ({ note, body }) {
     console.log(note, "Note from NoteCard")
     console.log(body, "body from NoteCard")
     return(
-        <div style={{fontSize: 45}}>
-            <h2>Note: { body }</h2>
-        </div>
+        <div>
+      <CKEditor
+        editor={DecoupledcEditor}
+        data="<p>Hello from CKEditor 5!</p>"
+        onInit={(editor) => {
+          console.log("Editor is ready to use!");
+          console.log(editor.ui.getEditableElement());
+          editor.ui
+            .getEditableElement()
+            .parentElement.append(editor.ui.view.toolbar.element);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+        }}
+        config={{
+          toolbarLocation: "bottom"
+        }}
+      />
+    </div>
     )
 }
 
